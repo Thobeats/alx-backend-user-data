f08498c69deb325cbe0c686b4cd8c7f7ac908e77
Iyanu Dayisi <tobiy23@gmail.com>
2024-09-04 03:27:05 +0100

    require auth

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -3,12 +3,22 @@
 """
 from flask import request
 from typing import List, TypeVar
+import re
 
 
 class Auth:
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        return False
+        if path is None or excluded_paths is None or excluded_paths == []:
+            return True
+        if path[-1] != '/':
+            path += '/'
+        for p in excluded_paths:
+            if p[-1] != '/':
+                p += '/'
+            if re.match(p, path):
+                return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         return None
