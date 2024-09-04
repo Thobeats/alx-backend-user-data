90a5e46b62a6469bd2aceb160d5249a82da9aff2
Iyanu Dayisi <tobiy23@gmail.com>
2024-09-04 02:41:42 +0100

    401 error

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -25,7 +25,6 @@ def stats() -> str:
     stats['users'] = User.count()
     return jsonify(stats)
 
-
 @app_views.route('/unauthorized', strict_slashes=False)
 def unauthorized() -> str:
     """ GET /api/v1/unauthorized
