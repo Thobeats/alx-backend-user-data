81dc539ec269b0950e39dd37fee64fc43eafd7de
Iyanu Dayisi <tobiy23@gmail.com>
2024-09-04 04:09:36 +0100

    require basic auth

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -40,3 +40,17 @@ class BasicAuth(Auth):
                 'base64')
         except Exception:
             return None
+
+    def extract_user_credentials(
+                                self,
+                                decoded_base64_authorization_header: str
+                                ) -> (str, str):
+        """Extract user credentials
+        """
+        if decoded_base64_authorization_header is None or type(
+                                                decoded_base64_authorization_header
+                                                ) is not str:
+            return (None, None)
+        if ':' not in decoded_base64_authorization_header:
+            return (None, None)
+        return tuple(decoded_base64_authorization_header.split(':', 1))
