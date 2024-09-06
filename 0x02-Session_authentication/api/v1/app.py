843fd9b57d9b2526427e699c01647fd10d81a7cb
Iyanu Dayisi <tobiy23@gmail.com>
2024-09-06 03:21:31 +0100

    update app

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -32,8 +32,11 @@ def before_request():
         return
     if auth.require_auth(request.path, ['/api/v1/status/',
                                         '/api/v1/unauthorized/',
-                                        '/api/v1/forbidden/']):
+                                        '/api/v1/forbidden/',
+                                        '/api/v1/auth_session/login/']):
         if not auth.authorization_header(request):
+            abort(401)
+        if not auth.session_cookie(request):
             abort(401)
         if not auth.current_user(request):
             abort(403)
