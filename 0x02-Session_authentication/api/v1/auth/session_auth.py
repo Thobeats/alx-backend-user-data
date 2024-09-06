5b7a94537d247cd32f772b495e35318a0401a1f9
Iyanu Dayisi <tobiy23@gmail.com>
2024-09-06 03:13:44 +0100

    retrieve user by session id

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -9,14 +9,14 @@ from uuid import uuid4
 class SessionAuth(Auth):
     """SessionAuth class
     """
-    user_id_by_session_id = {}
+    user_id_by_session_id: dict = {}
 
     def create_session(self, user_id: str = None) -> str:
         """Create session
         """
         if user_id is None or type(user_id) is not str:
             return None
-        session_id = uuid4()
+        session_id = str(uuid4())
         self.user_id_by_session_id[session_id] = user_id
         return session_id
 
