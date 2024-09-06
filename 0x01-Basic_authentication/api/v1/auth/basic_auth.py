54f63fb6c851059e7c5c27fc3e80e57e3208074e
Iyanu Dayisi <tobiy23@gmail.com>
2024-09-06 01:11:06 +0100

    current user

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -75,3 +75,19 @@ class BasicAuth(Auth):
             if user.is_valid_password(user_pwd):
                 return user
         return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """Current user
+        """
+        auth_header = self.authorization_header(request)
+        base64_header = self.extract_base64_authorization_header(
+                                                                auth_header
+                                                                )
+        decoded_header = self.decode_base64_authorization_header(
+                                                                base64_header
+                                                                )
+        user_credentials = self.extract_user_credentials(decoded_header)
+        return self.user_object_from_credentials(
+                                                user_credentials[0],
+                                                user_credentials[1]
+                                                )
