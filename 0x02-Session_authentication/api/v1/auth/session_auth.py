a175e6ba8f49c9445117561adad33383a855a098
Iyanu Dayisi <tobiy23@gmail.com>
2024-09-06 02:59:58 +0100

    crate session ID

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -2,14 +2,20 @@
 """
 Session Auth module
 """
-from flask import request
-from typing import List, TypeVar
-import re
 from api.v1.auth.auth import Auth
-from base64 import b64decode
-from models.user import User
+from uuid import uuid4
 
 
 class SessionAuth(Auth):
     """SessionAuth class
     """
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """Create session
+        """
+        if user_id is None or type(user_id) is not str:
+            return None
+        session_id = uuid4()
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
