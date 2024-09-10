081d9dc640a42933bcfce1e1381b6b81d1d875fd
Iyanu Dayisi <tobiy23@gmail.com>
2024-09-10 19:55:27 +0100

    validate login

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -26,6 +26,18 @@ class Auth:
             pass
         return self._db.add_user(email, _hash_password(password))
 
+    def valid_login(self, email: str, password: str) -> bool:
+        """Validate login Credentials"""
+        try:
+            user = self._db.find_user_by(email=email)
+            if not user:
+                return False
+            return bcrypt.checkpw(bytes(password, 'utf-8'),
+                                  user.hashed_password)
+        except NoResultFound:
+            pass
+        return False
+
 
 def _hash_password(password: str) -> bytes:
     """
