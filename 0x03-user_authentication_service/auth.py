bc694b5970d89e803b12760a7bcc9f61c63e679c
Iyanu Dayisi <tobiy23@gmail.com>
2024-09-10 23:35:49 +0100

    update password

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -83,6 +83,15 @@ class Auth:
         except NoResultFound:
             raise ValueError
 
+    def update_password(self, reset_token: str, password: str) -> None:
+        """Update password"""
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+            hashed_password = _hash_password(password)
+            self._db.update_user(user.id, hashed_password=hashed_password)
+        except NoResultFound:
+            raise ValueError
+
 
 def _generate_uuid() -> str:
     """
